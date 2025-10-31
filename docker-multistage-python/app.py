435d749b69572442fc1302a68fc4259b79c4bbd1
Shaikh Sufiyan <sufiyan.shaikh0114@gmail.com>
2025-10-31 16:57:01 +0530

    Update app.py

--- docker-multistage-python/app.py
+++ docker-multistage-python/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return " Dockerfile test successful! Running on Distroless Python."
+    return " Dockerfile Test Successful! Running on Distroless Python."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
